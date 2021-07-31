4df181a6bc7f351babb6f9afe8b2d101f9224c3d
mai.shu <ansiboy@163.com>
2021-07-31 15:58:32 +0800

    modify test

--- demo/modules/item-select-column.tsx
+++ demo/modules/item-select-column.tsx
@@ -1,6 +1,7 @@
 import { DataListPage } from "../../out/index";
 import { rules } from "maishu-dilu";
 import { createDataSource, Person } from "../data-source";
+import * as React from "react";
 
 export default class extends DataListPage<Person> {
     dataSource = createDataSource();
@@ -26,4 +27,19 @@ export default class extends DataListPage<Person> {
             }
         })
     ]
+    renderToolbarRight() {
+        let r = super.renderToolbarRight();
+        r.unshift(...[
+            <button key="btnTest" className="btn btn-primary btn-sm"
+                onClick={() => this.test()}>
+                <i className="fa fa-plus"></i>
+                <span>TEST</span>
+            </button>
+        ])
+        return r;
+    }
+
+    test() {
+        alert(this.selectItemColumn.selectedItems.length);
+    }
 }
